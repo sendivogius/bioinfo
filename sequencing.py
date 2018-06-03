1c8032b5bf2a476b3589bf9e630fad2855f37d4a
Tadeusz Satlawa <tadeusz.satlawa@ardigen.com>
2018-06-03 22:02:22 +0200

    Add part 1 of sequencing

--- sequencing.py
+++ sequencing.py
@@ -1,5 +1,23 @@
 import motifs
 
+
 def composition(dna, k):
-    pass
     return sorted(motifs.get_all_kmers(dna, k, ordered=True))
+
+
+def genome_from_sequence(kmers_sequence):
+    k = len(kmers_sequence[0])
+    return (''.join(kmers_sequence[::k])) + kmers_sequence[-1][len(kmers_sequence) // k:]
+
+
+def print_formatted(d):
+    for k in sorted(d.keys()):
+        print(k, '->', ','.join(d[k]))
+
+
+def graph_from_sequence(kmers_sequence):
+    k = len(kmers_sequence[0])
+    all_kmers = {kmer1: {kmer2 for kmer2 in kmers_sequence if kmer1[1:k] == kmer2[:k - 1]} for kmer1 in kmers_sequence}
+    v = {k: v for k, v in all_kmers.items() if v}
+    print_formatted(v)
+    # return v
