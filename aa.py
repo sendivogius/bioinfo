248f548286b06ace6708e7c3d2bea00381497048
sendivogius <sendivogius@gmail.com>
2018-06-28 09:06:02 +0200

    Add aminoacids code

--- aa.py
+++ aa.py
@@ -1,4 +1,7 @@
+import functools
 import itertools
+
+import math
 
 import motifs
 
@@ -67,11 +70,24 @@ rna_codons = {'AAA': 'K',
               'UUG': 'L',
               'UUU': 'F'}
 
+
+def read_dict_file(name):
+    str_dict = dict([line.split() for line in open(name)])
+    return {k: int(v) for (k, v) in str_dict.items()}
+
+
+aminoacids_mass_map = read_dict_file('data/integer_mass_table.txt')
+aminoacids_masses = list(sorted(set(aminoacids_mass_map.values())))  # todo why must be sorted?
+aminoacids = set(rna_codons.values()) - {''}
+
+
 def dna_to_rna(dna):
     return dna.replace('T', 'U')
 
+
 def rna_to_dna(rna):
     return rna.replace('U', 'T')
+
 
 def _reverse_dict(d):
     rev = dict()
@@ -104,14 +120,121 @@ def reverse_translate(aa):
 def get_encodings(dna, aa):
     rna = dna_to_rna(dna)
     rna_rev_comp = dna_to_rna(motifs.reverse_strand(dna))
-    aa_len = 3*len(aa)
+    aa_len = 3 * len(aa)
     encodings = [rna_to_dna(kmer) for kmer in motifs.get_all_kmers(rna, aa_len, True) if translate(kmer) == aa]
-    encodings_rev = [motifs.reverse_strand(rna_to_dna(kmer)) for kmer in motifs.get_all_kmers(rna_rev_comp, aa_len, True) if translate(kmer) == aa]
+    encodings_rev = [motifs.reverse_strand(rna_to_dna(kmer)) for kmer in
+                     motifs.get_all_kmers(rna_rev_comp, aa_len, True) if translate(kmer) == aa]
     return encodings + encodings_rev
 
 
+def get_peptide_mass(aa_str):
+    return sum(aminoacids_mass_map[a] for a in aa_str)
+
+
+def theoretical_spectrum(aa_seq):
+    l = len(aa_seq)
+    da = aa_seq * 2
+    peptides = [da[start:start + k] for k in range(1, l) for start in range(l)] + ['', aa_seq]
+    peptides_mass = [get_peptide_mass(pep) for pep in peptides]
+    return sorted(peptides_mass)
+
+
+def parent_mass(spectrum):
+    return max(spectrum)
+
+
+def expand_peptides(peptides):
+    return {p + a for p in peptides for a in aminoacids}
+
+
+def get_all_combinations_iterative(elements, target_value):
+    '''
+    Generates ... which sum to target
+    :param elements:
+    :param target_value:
+    :return:
+    '''
+    element_to_check = elements[0]
+    remaining_elements = elements[1:]
+    max_num_of_elem = target_value // element_to_check
+
+    if max_num_of_elem * element_to_check == target_value:
+        yield {element_to_check: max_num_of_elem}
+
+    if remaining_elements:
+        for i in range(max_num_of_elem):
+            for current_sol in get_all_combinations_iterative(remaining_elements, target_value - i * element_to_check):
+                if i:
+                    current_sol[element_to_check] = i
+                yield current_sol
+
+
+def _num_peptide_combinations(peptide_composition):
+    pep_len = sum(peptide_composition.values())
+    cc = functools.reduce(lambda x, y: x * y, (math.factorial(d) for d in peptide_composition.values()))
+    return int(math.factorial(pep_len) / cc)
+
+
+def number_of_peptides_with_mass(peptide_length):
+    comb = get_all_combinations_iterative(aminoacids_masses, peptide_length)
+    return sum(_num_peptide_combinations(x) for x in comb)
+
+
+def is_consistent(test_spec, target_spec):
+    return set(test_spec) <= set(target_spec)
+
+
+def cyclopeptide_sequencing_generator(spectrum):
+    peptides = {''}
+    while peptides:
+        matching_pep = set()
+        for pep in expand_peptides(peptides):
+            if get_peptide_mass(pep) == parent_mass(spectrum) and theoretical_spectrum(pep) == spectrum:
+                yield pep
+            if is_consistent(theoretical_spectrum(pep), spectrum):
+                matching_pep.add(pep)
+        peptides = matching_pep
+
+
+def mass_string(peptide):
+    return '-'.join([str(aminoacids_mass_map[aa]) for aa in peptide])
+
+
+def cyclopeptide_sequencing(spectrum, unique=False):
+    peptides = set(cyclopeptide_sequencing_generator(spectrum))
+    if not unique:
+        return peptides
+    return {mass_string(pep) for pep in peptides}
+
+
+
+def approx_pept_mass(start=100, stop=1000, step=2):
+    from scipy.optimize import curve_fit
+    import pandas as pd
+    import numpy as np
+    from tqdm import tqdm
+    import matplotlib.pyplot as plt
+
+    def approx_func(m, k, C):
+        return k * np.power(C, m)
+
+    series = pd.Series()
+    for i in tqdm(range(start, stop + 1, step)):
+        num = number_of_peptides_with_mass(i)
+        series.set_value(i, num)
+    series.to_csv('peptide_number_by_mass_approx.csv')
+
+    xdata = series.index.values
+    ydata = series.values
+    popt, pcov = curve_fit(approx_func, xdata, ydata, (0.00742696, 1.02820358))
+    print(popt)
+    plt.semilogy(xdata, ydata, 'b-', label='data')
+    plt.semilogy(xdata, approx_func(xdata, *popt), 'r-', label='approximation')
+    plt.xlabel('mass')
+    plt.ylabel('number of peptides')
+    plt.savefig('a.png')
+    plt.show()
+
+
 if __name__ == "__main__":
-    bacillus = ''.join((s.strip() for s in open('data\\Bacillus_brevis.txt').readlines()))
-    tyrocidineB1 = 'VKLFPWFNQY'
-    encodings = get_encodings(bacillus, tyrocidineB1)
-    print(encodings)
+    approx_pept_mass(200, 1500, 10)
--- aa_tests.py
+++ aa_tests.py
@@ -2,7 +2,6 @@ import unittest
 import aa
 
 
-
 class AaTests(unittest.TestCase):
     def test_translation(self):
         dna = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
@@ -12,25 +11,109 @@ class AaTests(unittest.TestCase):
     def test_reverse_translation(self):
         aa_str = 'MS'
         expected = {
-                    'AUGAGU',
-                    'AUGAGC',
-                    'AUGUCA',
-                    'AUGUCC',
-                    'AUGUCG',
-                    'AUGUCU'
-                    }
+            'AUGAGU',
+            'AUGAGC',
+            'AUGUCA',
+            'AUGUCC',
+            'AUGUCG',
+            'AUGUCU'
+        }
         self.assertEqual(expected, aa.reverse_translate(aa_str))
 
     def test_get_econdings(self):
         dna = 'ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA'
         aa_str = 'MA'
         expected = [
-                    'ATGGCC',
-                    'GGCCAT',
-                    'ATGGCC',
-                    ]
-        print(aa.get_encodings(dna, aa_str))
+            'ATGGCC',
+            'GGCCAT',
+            'ATGGCC',
+        ]
         self.assertEqual(sorted(expected), sorted(aa.get_encodings(dna, aa_str)))
+
+    def test_peptide_mass(self):
+        aa_str = 'NQE'
+        expected = 371
+        self.assertEqual(expected, aa.get_peptide_mass(aa_str))
+
+    def test_peptide_mass_empty(self):
+        aa_str = ''
+        expected = 0
+        self.assertEqual(expected, aa.get_peptide_mass(aa_str))
+
+    def test_aa_spectrum(self):
+        aa_str = 'LEQN'
+        expected = [0, 113, 114, 128, 129, 227, 242, 242, 257, 355, 356, 370, 371, 484]
+        self.assertEqual(expected, aa.theoretical_spectrum(aa_str))
+
+    def test_parent_mass(self):
+        spectrum = [0, 113, 114, 128, 129, 227, 242, 242, 257, 355, 356, 370, 371, 484]
+        expected = 484
+        self.assertEqual(expected, aa.parent_mass(spectrum))
+
+    def test_expand_peptides_empty(self):
+        peptides = {''}
+        expected = {"A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"}
+        self.assertEqual(expected, aa.expand_peptides(peptides))
+
+    def test_expand_peptides(self):
+        peptides = {'A', 'DD'}
+        expected = {"AA", "AR", "AN", "AD", "AC", "AE", "AQ", "AG", "AH", "AI", "AL", "AK", "AM", "AF", "AP", "AS",
+                    "AT", "AW", "AY", "AV",
+                    "DDA", "DDR", "DDN", "DDD", "DDC", "DDE", "DDQ", "DDG", "DDH", "DDI", "DDL", "DDK", "DDM", "DDF",
+                    "DDP", "DDS", "DDT", "DDW", "DDY", "DDV"}
+        self.assertEqual(expected, aa.expand_peptides(peptides))
+
+    def test_subpeptides_number(self):
+        peptide_length = 841
+        expected = 112663579
+        self.assertEqual(expected, aa.number_of_peptides_with_mass(peptide_length))
+
+    def test_is_not_consistent(self):
+        test_peptide = 'VKF'
+        test_spec = aa.theoretical_spectrum(test_peptide)
+        target_spec = [0, 97, 99, 113, 114, 128, 128, 147, 147, 163, 186, 227, 241, 242, 244, 260, 261, 262, 283, 291,
+                       333, 340, 357, 388, 389, 390, 390, 405, 430, 430, 447, 485, 487, 503, 504, 518, 543, 544, 552,
+                       575, 577, 584, 631, 632, 650, 651, 671, 672, 690, 691, 738, 745, 747, 770, 778, 779, 804, 818,
+                       819, 835, 837, 875, 892, 892, 917, 932, 932, 933, 934, 965, 982, 989, 1031, 1039, 1060, 1061,
+                       1062, 1078, 1080, 1081, 1095, 1136, 1159, 1175, 1175, 1194, 1194, 1208, 1209, 1223, 1225, 1322]
+        self.assertFalse(aa.is_consistent(test_spec, target_spec))
+
+    def test_is_consistent(self):
+        test_peptide = 'VKY'
+        test_spec = aa.theoretical_spectrum(test_peptide)
+        target_spec = [0, 97, 99, 113, 114, 128, 128, 147, 147, 163, 186, 227, 241, 242, 244, 260, 261, 262, 283, 291,
+                       333, 340, 357, 388, 389, 390, 390, 405, 430, 430, 447, 485, 487, 503, 504, 518, 543, 544, 552,
+                       575, 577, 584, 631, 632, 650, 651, 671, 672, 690, 691, 738, 745, 747, 770, 778, 779, 804, 818,
+                       819, 835, 837, 875, 892, 892, 917, 932, 932, 933, 934, 965, 982, 989, 1031, 1039, 1060, 1061,
+                       1062, 1078, 1080, 1081, 1095, 1136, 1159, 1175, 1175, 1194, 1194, 1208, 1209, 1223, 1225, 1322]
+        self.assertTrue(aa.is_consistent(test_spec, target_spec))
+
+    def test_mass_string(self):
+        peptide = 'IWK'
+        expected = '128-111-111'
+        self.assertTrue(expected, aa.mass_string(peptide))
+
+    def test_cyclopeptide_sequencing_unique(self):
+        spectrum = [0,71,97,99,103,113,113,114,115,131,137,196,200,202,208,214,226,227,228,240,245,299,311,311,316,327,337,339,340,341,358,408,414,424,429,436,440,442,453,455,471,507,527,537,539,542,551,554,556,566,586,622,638,640,651,653,657,664,669,679,685,735,752,753,754,756,766,777,782,782,794,848,853,865,866,867,879,885,891,893,897,956,962,978,979,980,980,990,994,996,1022,1093]
+        print( aa.cyclopeptide_sequencing(spectrum, unique=True))
+        self.assertEqual({'113-128-186', '113-186-128', '128-113-186', '128-186-113', '186-113-128', '186-128-113'},
+                         aa.cyclopeptide_sequencing(spectrum, unique=True))
+
+    def test_cyclopeptide_sequencing(self):
+        spectrum = [11,71,87,97,99,101,115,115,128,128,129,172,184,186,186,198,227,243,243,244,257,269,283,285,287,314,314,342,356,372,372,372,384,384,411,415,429,443,455,471,471,487,500,512,512,526,544,558,558,570,583,599,599,615,627,641,655,659,686,686,698,698,698,714,728,756,756,783,785,787,801,813,826,827,827,843,872,884,884,886,898,941,942,942,955,955,969,971,973,983,999,1070]
+
+        self.assertEqual(
+            {'IWQ', 'WQL', 'WKL', 'QWL', 'IQW', 'KLW', 'LWK', 'WQI', 'LWQ', 'WLQ', 'KIW', 'WIQ', 'LKW', 'IKW', 'KWI',
+             'WIK', 'LQW', 'QWI', 'IWK', 'KWL', 'QLW', 'WLK', 'WKI', 'QIW'}
+            , aa.cyclopeptide_sequencing(spectrum, unique=False))
+
+    def test_a(self):
+        pep = 'CHAMNLLDVP'
+        spec = aa.theoretical_spectrum(pep)
+
+        target = [0,71,97,99,103,113,113,114,115,131,137,196,200,202,208,214,226,227,228,240,245,299,311,311,316,327,337,339,340,341,358,408,414,424,429,436,440,442,453,455,471,507,527,537,539,542,551,554,556,566,586,622,638,640,651,653,657,664,669,679,685,735,752,753,754,756,766,777,782,782,794,848,853,865,866,867,879,885,891,893,897,956,962,978,979,980,980,990,994,996,1022,1093]
+        cons = aa.is_consistent(spec, target)
+        self.assertTrue(cons)
 
 if __name__ == '__main__':
     unittest.main()
